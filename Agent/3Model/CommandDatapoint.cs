2caa5825ea4ec6becf1713bab70c7dbc0b4b3fe9
Samir Kahvedzic <samir.kahvedzic1@gmail.com>
2017-03-16 02:26:22 +0000

    Added commands

---
+++ Agent/3Model/CommandDatapoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RAT.ZTry;
+
+namespace ConsoleApplication1
+{
+    public class CommandDatapoint
+    {
+        public DateTime ExpireTime { get; set; }
+        public string Command { get; set; }
+        public string ProcessName { get; set; }
+        public CommandType CommandType { get; set; }
+    }
+}
--- Agent/4Services/IoT/SendTelemetry.cs
+++ Agent/4Services/IoT/SendTelemetry.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,9 @@ using Agent._4Services;
 using ConsoleApplication1;
 using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using RAT.ZTry;
 
 namespace MyDeviceTest
 {
@@ -25,6 +29,8 @@ namespace MyDeviceTest
         //LFqCq06kB8m/7abwakvP1tBjo8zLWATyN3soGLogOew= device2
         PerformanceCounters information;
         private TelemetryDatapoint telemetry;
+        private static CommandDatapoint command;
+        private static string newString;
         //Sending messages to the IoT hub
         private async Task SendDeviceToCloudMessagesAsync()
         {
@@ -74,10 +80,100 @@ namespace MyDeviceTest
                 //Printing out message
                 Console.WriteLine(messageString);
                 
-                Task.Delay(450).Wait();
+                Task.Delay(550).Wait();
             }
         }
 
+        private static async void ReceiveC2dAsync()
+        {
+            Console.WriteLine("\nReceiving cloud to device messages from service");
+            while (true)
+            {
+                Message receivedMessage = await deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                string JsonString = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                string newString = JsonString.Substring(2);
+                command = JsonConvert.DeserializeObject<CommandDatapoint>(newString);
+
+                ExacuteCommand();
+                await deviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+
+        public static void ExacuteCommand()
+        {
+            //If the command has not expired
+            if (command.ExpireTime.AddMinutes(1) > DateTime.Now)
+            {
+                if (command.CommandType == CommandType.CloseProcess)
+                {
+                    StopProcess(command.ProcessName);
+                }
+                else if (command.CommandType == CommandType.Hibernate)
+                {
+                    //Hibernates the computer
+                    System.Diagnostics.Debug.WriteLine("Command Received:" + command);
+                    SetSuspendState(true, true, true);
+                }
+                else if (command.CommandType == CommandType.ShutDown)
+                {
+                    System.Diagnostics.Debug.WriteLine("Command Received:" + command);
+                    Process.Start("shutdown", "/s /t 0");
+                    // starts the shutdown application                                      
+                    // the argument /s is to shut down the computer                                   
+                    // the argument /t 0 is to tell the process that                              
+                    // the specified operation needs to be completed 
+                    // after 0 seconds
+                }
+                else if (command.CommandType == CommandType.Restart)
+                {
+                    System.Diagnostics.Debug.WriteLine("Command Received:" + command);
+                    Process.Start("shutdown", "/r /t 0"); // the argument /r is to restart the computer
+                }
+                else if (command.CommandType == CommandType.Logoff)
+                {
+                    System.Diagnostics.Debug.WriteLine("Command Received:" + command);
+                    ExitWindowsEx(0, 0);
+                }
+                else if (command.CommandType == CommandType.Sleep)
+                {
+                    //Puts the computer into sleep mode
+                    System.Diagnostics.Debug.WriteLine("Command Received:" + command);
+                    SetSuspendState(false, true, true);
+                }
+                else if (command.CommandType == CommandType.Lock)
+                {
+                    System.Diagnostics.Debug.WriteLine("Lock:" + command);
+                    LockWorkStation();
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Command Expired" + newString + DateTime.Now);
+            }
+        }
+
+
+        public static void StopProcess(string processName)
+        {
+            //Kills a process if it is found
+            Process[] listOfProcesses = Process.GetProcesses();
+            for (int i = 0; i < listOfProcesses.Length; i++)
+            {
+                if (listOfProcesses[i].ProcessName == processName)
+                {
+                    try
+                    {
+                        listOfProcesses[i].Kill();
+                    }
+                    catch (Exception e)
+                    {
+                        System.Diagnostics.Debug.WriteLine(e);
+                    }
+                }
+            }
+        }
         public async void stop()
         {
             kill = true;
@@ -95,8 +191,19 @@ namespace MyDeviceTest
             worker.WorkerReportsProgress = true;
             worker.DoWork += worker_send;
             worker.RunWorkerAsync();
+            ReceiveC2dAsync();
             Console.ReadLine();
         }
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+        [DllImport("user32")]
+        public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        [DllImport("user32")]
+        public static extern void LockWorkStation();
+
+        // Standby
         void worker_send(object sender, DoWorkEventArgs e)
         {
             SendDeviceToCloudMessagesAsync();
